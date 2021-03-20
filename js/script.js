2126116089abb9dfa600d16f60d9a063cac265c2
Sergey <44847286+WSERW@users.noreply.github.com>
2021-03-20 19:48:18 +0300

    parallax and animations

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ window.onscroll = function(){
         navbar.style.background = 'transparent'
     }
 }
+
+let headerScene = document.querySelector('header');
+let headerParalax = new Parallax(headerScene);
+let welcomeScene = document.querySelector('.welcome');
+let welcomeParalax = new Parallax(welcomeScene);
+let teachersScene = document.querySelector('.teachers');
+let teachersParalax = new Parallax(teachersScene);
+let contactsGirlScene = document.querySelector('.contacts_girl_scene');
+let contactsGirlParalax = new Parallax(contactsGirlScene);
+let contactsTriangleScene = document.querySelector('.contacts_triangle_scene');
+let contactsTriangleParalax = new Parallax(contactsTriangleScene);
